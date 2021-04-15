88d32417829cd73c259110a5bce1ba0c36d6de7b
Kateti Mareko <katetimareko@codenertapps.com>
2021-04-16 11:24:15 +1200

    Add student route to the server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,15 @@
 const path = require('path')
 const express = require('express')
-<<<<<<< HEAD
-const studentRouter = require('./routes/students')
-=======
 const celebPhotosroutes = require('./routes/celebPhotosRoutes')
->>>>>>> 5adb0f115f6e69b06932f3c12f481f051221dd11
+const studentRoute = require('./routes/students')
+
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-<<<<<<< HEAD
-server.use('/students', studentRouter)
-=======
 server.use('/', celebPhotosroutes)
->>>>>>> 5adb0f115f6e69b06932f3c12f481f051221dd11
+server.use('/students', studentRoute)
 
 module.exports = server
