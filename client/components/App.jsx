0b8787ae78085ef67ff80504b3a34897d17598ff
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-04-16 10:16:08 +1200

    tailwind config and react set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
+function App () {
+    return (
+        <>
+        <p>Hello</p>
+        <Route path='/Results' component={Results} />
+        </>
+    )
+}
+
 export default App
