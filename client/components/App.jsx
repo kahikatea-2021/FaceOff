b331216fedcb7660bd2934a23cf29cf6cf505bca
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-04-16 11:45:26 +1200

    app and results working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,8 +6,20 @@ import Results from './Results'
 function App () {
   return (
     <div>
-      <Header />
-      <Link to="/" component={Results} />
+      <body className="bg-pink-300">
+        <Header />
+        <Route path='/Results' component={Results} />
+        <div className="grid grid-cols-2">
+          <div className=" justify-self-end">
+            <img src="/images/will.png"></img>
+          </div>
+          <div className="flex items-center">
+            <Link to="/Results" >
+              <button className="bg-pink-500 w-64 h-64 hover:bg-pink-700 rounded-full">GO!</button>
+            </Link>
+          </div>
+        </div>
+      </body>
     </div>
   )
 }
